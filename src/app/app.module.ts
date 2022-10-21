bb94075a9c0df5c0eee83d3fc1747f1b84f16222
6299 <kento.puleo@pax.ch>
2022-10-21 10:26:32 +0200

    Add new Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { ListdoneComponent } from './components/listdone/listdone.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListdoneComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'listdone', component: ListdoneComponent},
+      { path: 'list', component: ListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
